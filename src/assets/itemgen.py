ba79796afb4d29fffe0b4189f0b16b69574c2112
Hui <jeffrey.x.hui@gmail.com>
2020-06-12 02:02:29 -0700

    importing correctly

--- src/assets/itemgen.py
+++ src/assets/itemgen.py
@@ -1,1 +1,10 @@
-from items import Weapon, Armor, Potion
+import sys
+import os
+
+items = os.path.abspath( os.path.dirname(__file__) + '/entities' )
+sys.path.insert(0, items)
+
+
+from items import Weapon, Potion, Armor
+
+print(Weapon)
--- src/assets/mobgen.py
+++ src/assets/mobgen.py
@@ -1,2 +1,6 @@
+import os
+
+mobs = os.path.abspath(os.path.abspath('.') + '/src/assets/entities/mobs.py')
 from mobs import Monster
 
+print(Monster)
